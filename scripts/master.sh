7d869295aecac471516de776e59572f1ef268ceb
Tiago Santos <tfsantosbr@gmail.com>
2020-06-24 21:18:08 -0300

    ok

--- scripts/master.sh
+++ scripts/master.sh
@@ -6,6 +6,7 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown -R $USER $HOME/.kube
 
+# instalando o network
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # kubeadm token create --print-join-command
