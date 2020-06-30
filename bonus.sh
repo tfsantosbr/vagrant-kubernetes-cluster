f58f9d8940b31a4d5f3e6f77c22f8b415976ee82
Tiago Santos <tfsantosbr@gmail.com>
2020-06-30 12:10:34 -0300

    ok

--- bonus.sh
+++ bonus.sh
@@ -1,3 +1,7 @@
+
+kubeadm init --apiserver-cert-extra-sans=<PUBLIC_IP>
+
+
 # um dos dois no master
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
@@ -16,3 +20,10 @@ backend http_back
   balance roundrobin
   server kube <worker-node1-ip>:80
   server kube <worker-node2-ip>:80
+
+# certs
+
+rm /etc/kubernetes/pki/apiserver.*
+kubeadm init phase certs all --apiserver-advertise-address=0.0.0.0 --apiserver-cert-extra-sans=<PUBLIC_IP>
+docker rm -f `docker ps -q -f 'name=k8s_kube-apiserver*'`
+systemctl restart kubelet
